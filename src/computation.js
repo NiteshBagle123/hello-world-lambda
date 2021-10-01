4ed9921c2123c3844831c93c05de798973843cdf
Nitesh Bagle <nbagle@egain.com>
2021-10-01 04:16:57 -0700

    test cases set up

---
+++ src/computation.js
@@ -0,0 +1,11 @@
+const sum = (a = 0, b = 0) => a + b;
+
+const difference = (a = 0, b = 0) => a - b;
+
+const multiply = (a = 0, b = 0) => a * b;
+
+module.exports = {
+  sum,
+  difference,
+  multiply
+};
---
+++ tests/computation.test.js
@@ -0,0 +1,33 @@
+const computation = require('../src/computation');
+
+describe('test computation.js', () => {
+  describe('test sum function', () => {
+    test('should test sum default parameter values', () => {
+      expect(computation.sum()).toBe(0);
+    });
+
+    test('should test sum actual values', () => {
+      expect(computation.sum(1, 1)).toBe(2);
+    });
+  });
+
+  describe('test difference function', () => {
+    test('should test difference default parameter values', () => {
+      expect(computation.difference()).toBe(0);
+    });
+
+    test('should test difference actual values', () => {
+      expect(computation.difference(2, 1)).toBe(1);
+    });
+  });
+
+  describe('test multiply function', () => {
+    test('should test multiply default parameter values', () => {
+      expect(computation.multiply()).toBe(0);
+    });
+
+    test('should test multiply actual values', () => {
+      expect(computation.multiply(2, 1)).toBe(2);
+    });
+  });
+});
