99daea35338785553bd2e793172474c3c715b100
Nitesh Bagle <niteshbagle43@gmail.com>
2021-10-02 20:56:33 +0530

    wip

--- src/computation.js
+++ src/computation.js
@@ -2,7 +2,7 @@ const sum = (a = 0, b = 0) => a + b;
 
 const difference = (a = 0, b = 0) => a - b;
 
-const multiply = (a = 0, b = 0) => a * b * 2;
+const multiply = (a = 0, b = 0) => a * b;
 
 module.exports = {
   sum,
