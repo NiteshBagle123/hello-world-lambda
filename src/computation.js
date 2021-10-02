fb8ed7de7a6140a9dd50261154fafcb5af96ec96
Nitesh Bagle <niteshbagle43@gmail.com>
2021-10-02 21:01:20 +0530

    wip

--- src/computation.js
+++ src/computation.js
@@ -4,6 +4,9 @@ const difference = (a = 0, b = 0) => a - b;
 
 const multiply = (a = 0, b = 0) => a * b;
 
+// eslint-disable-next-line no-console
+console.log('123');
+
 module.exports = {
   sum,
   difference,
