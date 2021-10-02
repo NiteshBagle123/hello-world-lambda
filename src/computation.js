a7d24ddfce3178c7a589711e91016ee7440a6783
Nitesh Bagle <niteshbagle43@gmail.com>
2021-10-02 19:25:56 +0530

    husky test

--- src/computation.js
+++ src/computation.js
@@ -1,8 +1,8 @@
-const sum = (     a = 0, b = 0) => a + b;
+const sum = (a = 0, b = 0) => a + b;
 
 const difference = (a = 0, b = 0) => a - b;
 
-const multiply = (a = 0, b = 0) => a * b;
+const multiply = (      a = 0, b = 0) => a * b;
 
 module.exports = {
   sum,
