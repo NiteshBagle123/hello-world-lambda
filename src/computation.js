0a16f83f32d356732fa31ea1b6e2dd9ea5324992
Nitesh Bagle <nbagle@egain.com>
2021-10-04 00:26:19 -0700

    wip

--- src/computation.js
+++ src/computation.js
@@ -1,11 +1,8 @@
-/* eslint-disable no-console */
 const sum = (a = 0, b = 0) => a + b;
 
 const difference = (a = 0, b = 0) => a - b;
 
 const multiply = (a = 0, b = 0) => a * b;
-
-console.log('Testing');
 
 module.exports = {
   sum,
